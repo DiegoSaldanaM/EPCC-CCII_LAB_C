d8069c031001cfb7a0a2ce88079004a6fb2df61d
DiegoSaldanaM <106556678+DiegoSaldanaM@users.noreply.github.com>
2022-06-13 21:58:34 -0500

    Add files via upload

---
+++ Lab_10/Ejercicio_2/list.cpp
@@ -0,0 +1,53 @@
+#include "list.h"
+#include <ctime>
+using namespace std;
+
+template<typename T>
+Lista<T>::Lista(){
+	m_num_nodes = 0;
+	m_head = NULL;
+}
+
+template<typename T>
+void Lista<T>::agregar_inicio(T dato_){
+	Nodo<T> *new_node = new Nodo<T> (dato_);
+	Nodo<T> *temp = m_head;
+	
+	if (!m_head) {
+		m_head = new_node;
+	} else {
+		new_node->siguiente = m_head;
+		m_head = new_node;
+		
+		while (temp) {
+			temp = temp->siguiente;
+		}
+	}
+	m_num_nodes++;
+}
+
+template<typename T>
+void Lista<T>::elementos_random(int tam){
+	srand(time(NULL));
+	for (int i = 0; i < tam; i++) {
+		agregar_inicio(rand() % 100);
+	}
+}
+
+template<typename T>
+void Lista<T>::imprimir(){
+	Nodo<T> *temp = m_head;
+	if (!m_head) {
+		cout << "Lista vacia: "<< endl;
+	} else {
+		while (temp) {
+			temp->imprimir();
+			if (!temp->siguiente) cout << "NULL";
+			temp = temp->siguiente;
+		}
+	}
+	cout << endl << endl;
+}
+
+template<typename T>
+Lista<T>::~Lista() {}
---
+++ Lab_10/Ejercicio_2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include "list.h"
+#include "list.cpp"
+
+using namespace std;
+
+int main(){
+	Lista<int> lista_1;
+	Lista<int> lista_2;
+	int elem;
+	
+	int tam;
+	int posicion;
+	
+	cout << "Indique el tamaño de la lista enlazada: " << endl;
+	cin >> tam;
+	
+	lista_1.elementos_random(tam);
+	
+	cout << "La lista inicial es: " << endl;
+	lista_1.imprimir();
+	
+	cout << "Ingresar elemento en la primera posición: " << endl;
+	cin >> elem;
+	lista_1.agregar_inicio(elem);
+	lista_1.imprimir();
+	
+	return 0;	
+}
